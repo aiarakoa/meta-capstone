61fdd0f2bbc1d28ca948187f920887583fb70d71
diario <diario@VisionDX.vdx.es>
2024-04-01 21:53:34 +0200

    Chakra not working

--- src/App.js
+++ src/App.js
@@ -7,19 +7,25 @@ import MenuPage from './components/MenuPage';
 import ReservePage from './components/ReservePage';
 import OrderPage from './components/OrderPage';
 import LoginPage from './components/LoginPage';
+import { AlertProvider } from "./context/alertContext";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path = "/"         element = {<HomePage />} />
-        <Route path = "/about"    element = {<AboutPage />} />
-        <Route path = "/menu"     element = {<MenuPage />} />
-        <Route path = "/reserve"  element = {<ReservePage />} />
-        <Route path = "/order"    element = {<OrderPage />} />
-        <Route path = "/login"    element = {<LoginPage />} />
-      </Routes>
+      <ChakraProvider>
+        <AlertProvider>
+          <Header />
+          <Routes>
+            <Route path = "/"         element = {<HomePage />} />
+            <Route path = "/about"    element = {<AboutPage />} />
+            <Route path = "/menu"     element = {<MenuPage />} />
+            <Route path = "/reserve"  element = {<ReservePage />} />
+            <Route path = "/order"    element = {<OrderPage />} />
+            <Route path = "/login"    element = {<LoginPage />} />
+          </Routes>
+        </AlertProvider>
+      </ChakraProvider>
     </>
   );
 }
--- src/components/ReserveForm.js
+++ src/components/ReserveForm.js
@@ -1,9 +1,81 @@
 import './ReserveForm.css';
+import React, {useEffect} from "react";
+import { useFormik } from "formik";
+/* import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Select,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+ */import * as Yup from 'yup';
+import useSubmit from "../hooks/useSubmit";
+import {useAlertContext} from "../context/alertContext";
+
+function doNothing() {}
+
+function destructureResponse(response) {
+    let type, message   =   null;
+    response == null                            ?   type = ''                                                   :   type = response.type;
+    response == null                            ?   message = 'Something went wrong, please try again later!'   :   message = response.message;
+    return [type, message];
+}
 
 function ReserveForm() {
+    const {isLoading, response, submit} = useSubmit();
+    const {onOpen} = useAlertContext();
+/*     const formik = useFormik({
+        initialValues: {
+          reservationDate: '',
+          reservationTime: '',
+          reservationNumGuests: '',
+          reservationOccasion: '',
+          reservationUserName: '',
+          reservationUserMail: '',
+          reservationUserPhone: '',
+        },
+        onSubmit: (values, actions) => {
+            submit("/", {firstName: values.firstName});
+            let [type, message]         =   destructureResponse(response);
+            onOpen(type, message);
+            response.type === 'error'   ?   doNothing()                                                     :   actions.resetForm({
+                values: {
+                    reservationDate: '',
+                    reservationTime: '',
+                    reservationNumGuests: '',
+                    reservationOccasion: '',  
+                    reservationUserName: '',  
+                    reservationUserMail: '',  
+                    reservationUserPhone: '',  
+                }
+            });
+        },
+        validationSchema: Yup.object({}).shape({ */
+/*
+          firstName: Yup.string().required('Required'),
+          email: Yup.string().email('Invalid email address').required('Required'),
+          type: Yup.string().default(undefined).optional().nullable(),
+          comment: Yup.string().min(25, 'Must be at least 25 characters').required('Required'),
+*/
+/*             reservationDate: Yup.string().required('Required'),
+            reservationTime: Yup.string().required('Required'),
+            reservationNumGuests: Yup.string().required('Required'),
+            reservationOccasion: Yup.string().required('Required'),  
+            reservationUserName: Yup.string().required('Required'),  
+            reservationUserMail: Yup.string().required('Required'),  
+            reservationUserPhone: Yup.string().required('Required'),  
+        }),
+    }); */
+
     return (
         <>
-            <form>
+            <span>Hiya!</span>
+{/*             <form>
                 <label for="res-date">Choose date</label>
                 <input type="date" id="res-date" />
                 <label for="res-time">Choose time</label>
@@ -24,6 +96,7 @@ function ReserveForm() {
                 </select>
                 <input type="submit" value="Make Your reservation" />
             </form>
+ */}
         </>
     );
 }
