14f649b52459d481a4fc41bd14de0e6b2647e066
Aiarakoa <aiarakoa@yahoo.es>
2024-06-23 17:17:32 +0200

    Try carousel

--- src/components/HomeTestimonials.js
+++ src/components/HomeTestimonials.js
@@ -1,21 +1,42 @@
 import './HomeTestimonials.css';
 import Testimonial from './Testimonial';
+import {useState} from 'react';
 
 function HomeTestimonials() {
+    const userNames                     =   ['Bérénice', 'Clifford', 'Ibrahima', 'Tilly', 'Sarai'];
+    const [userIndex, setUserIndex]     =   useState(0);
+    const shiftTestimonialsLeft         =   () => {
+        setUserIndex(userIndex + 1);
+    }
+
+    const shiftTestimonialsRight        =   () => {
+        setUserIndex(userIndex - 1);
+    }
+
+    const evaluateUserName              =   (offset) => {
+        return userNames[evaluateUserRealIndex(offset)];
+    }
+
+    const evaluateUserRealIndex         =   (offset) => {
+        return (((userIndex + offset) % userNames.length) + userNames.length) % userNames.length;
+    }
+
     return (
         <>
-            <section            className = "home-testimonials">
+            <section            className   =   "home-testimonials">
                 <h3>
                     Testimonials
                 </h3>
-                <button         className = "home-testimonials-left-arrow"      aria-label = "Click to access a new testimonial to the left">
+                <button         className   =   "home-testimonials-left-arrow"      aria-label  =   "Click to access a new testimonial to the left"
+                                onClick     =   {shiftTestimonialsLeft}>
                     
                 </button>
-                <Testimonial    userName = 'Bérénice' />
-                <Testimonial    userName = 'Clifford' />
-                <Testimonial    userName = 'Ibrahima' />
-                <Testimonial    userName = 'Tilly' />
-                <button         className = "home-testimonials-right-arrow"     aria-label = "Click to access a new testimonial to the right">
+                <Testimonial    userName = {evaluateUserName(0)} />
+                <Testimonial    userName = {evaluateUserName(1)} />
+                <Testimonial    userName = {evaluateUserName(2)} />
+                <Testimonial    userName = {evaluateUserName(3)} />
+                <button         className   =   "home-testimonials-right-arrow"     aria-label  =   "Click to access a new testimonial to the right"
+                                onClick     =   {shiftTestimonialsRight}>
 
                 </button>
             </section>
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -2,16 +2,18 @@ import './Testimonial.css';
 import bereniceImage from '../persona-berenice.png';
 import cliffordImage from '../persona-clifford.png';
 import ibrahimaImage from '../persona-ibrahima.png';
+import saraiImage from '../persona-sarai.png';
 import tillyImage from '../persona-tilly.png';
 
 function Testimonial(props) {
-    const images        = {'Bérénice': bereniceImage, 'Clifford': cliffordImage, 'Ibrahima': ibrahimaImage, 'Tilly': tillyImage};
-    const altTexts      = {'Bérénice': 'Meet Bérénice', 'Clifford': 'Meet Clifford', 'Ibrahima': 'Meet Ibrahima', 'Tilly': 'Meet Tilly'};
+    const images        = {'Bérénice': bereniceImage, 'Clifford': cliffordImage, 'Ibrahima': ibrahimaImage, 'Tilly': tillyImage, 'Sarai': saraiImage};
+    const altTexts      = {'Bérénice': 'Meet Bérénice', 'Clifford': 'Meet Clifford', 'Ibrahima': 'Meet Ibrahima', 'Tilly': 'Meet Tilly', 'Sarai': 'Meet Sarai'};
     const reviewTexts   = {
         'Bérénice': "“Their vegan offer is amazing. In addition, it’s so easy to reserve a table with them, I can always get what I need. They treat their customers with love, feeding our bellies and souls”",
         'Clifford': "“I enjoy eating at Little Lemon but, as I have a sweet tooth, what I love the most is their desserts, especially that lemon pie... I swear I’ll steal their recipe to bake it at home! xD I love these guys :-D”",
         'Ibrahima': "“Love their Mediterranean food, especially fish, and they always have good offers for frequent customers. Their delivery service is top notch, they got a client for life in me!”",
         'Tilly': "“Delicious food and the best delivery service in Chicago, so easy to order online, so fast to deliver! And food comes as if it were just been taken out of the kitchen, superb!”",
+        'Sarai': "“Every time I eat their food is like being at home in Spain, it is so authentic and savory... and I eat there more often than I expected because they have affordable menus and great offers”",
     }
     return (
         <>
